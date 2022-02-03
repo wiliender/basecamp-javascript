9a612dfe7b8b75cd2166dfa40c8ea4e4a7c2f420
Wiliender <wiliendersilva@gmail.com>
2022-02-02 21:12:28 -0300

    adding filter

---
+++ map-filter-reduce/filter/filtraPares.js
@@ -0,0 +1,9 @@
+function filtraPares(arr) {
+	if (!arr || !arr.length) return;
+
+	const filteredArr = arr.filter((item) => item % 2 === 0);
+
+	return filteredArr;
+}
+
+console.log(filtraPares([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
